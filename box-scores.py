04173f8d9c803153d5e45c98951e0c15b7b54f83
Kevin Schroeder <kschroeder@neboagency.com>
2020-01-30 16:28:59 -0500

    Added abbrivations scrape

--- box-scores.py
+++ box-scores.py
@@ -12,28 +12,20 @@ game_table = soup.findAll("div", {"class": "game_summary"})
 for game in game_table:
     table = []
     winning_team = game.find("tr", {"class": "winner"}).find("a").text
-    winning_team_score = game.find("tr", {
-        "class": "winner"
-    }).find("td", {
-        "class": "right"
-    }).text
+    winning_team_abv = game.find("tr", {"class": "winner"}).find("a")['href'].split('/')[2]
+    winning_team_score = game.find("tr", {"class": "winner"}).find("td", {"class": "right"}).text
     losing_team = game.find("tr", {"class": "loser"}).find("a").text
-    losing_team_score = game.find("tr", {
-        "class": "loser"
-    }).find("td", {
-        "class": "right"
-    }).text
+    losing_team_abv = game.find("tr", {"class": "loser"}).find("a")['href'].split('/')[2]
+    losing_team_score = game.find("tr", {"class": "loser"}).find("td", {"class": "right"}).text
 
-    game_link = 'https://www.basketball-reference.com/' + game.find(
-        "td", {
-            "class": "gamelink"
-        }).find('a')['href']
+    print(winning_team_abv + " " + losing_team_abv)
+
+    game_link = 'https://www.basketball-reference.com/' + game.find("td", {"class": "gamelink"}).find('a')['href']
     game_page = requests.get(game_link)
     game_soup = BeautifulSoup(game_page.content, 'html.parser')
 
     stats_table = game_soup.find("table", {"class": "stats_table"})
-    header_elements = stats_table.find_all(
-        'th', {"data-over-header": "Basic Box Score Stats"})
+    header_elements = stats_table.find_all('th', {"data-over-header": "Basic Box Score Stats"})
     headers = []
     for head in header_elements:
         headers.append(head.text)
@@ -68,6 +60,6 @@ for game in game_table:
     #     for player in player_stats_elements:
     #         current_player_row.append(player.text)
 
-    print(winning_team + " " + winning_team_score + " || " + losing_team +
-          ' ' + losing_team_score)
-    print(tabulate(table, headers=headers, tablefmt="psql"))
+    # print(winning_team + " " + winning_team_score + " || " + losing_team +
+    #       ' ' + losing_team_score)
+    # print(tabulate(table, headers=headers, tablefmt="psql"))
